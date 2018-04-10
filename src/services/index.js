c9225b13000aa7f6dbfb3300c72338ca4bf93b20
nacmonad <macnomad84@gmail.com>
2018-04-10 16:25:22 -0400

    basic setup, and services registered

---
+++ src/services/index.js
@@ -0,0 +1,7 @@
+const responses = require('./responses/responses.service.js');
+const questions = require('./questions/questions.service.js');
+// eslint-disable-next-line no-unused-vars
+module.exports = function (app) {
+  app.configure(responses);
+  app.configure(questions);
+};
