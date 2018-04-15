2887d120587cc4d4e33cb61cd1ca62eb8f1da644
nacmonad <macnomad84@gmail.com>
2018-04-15 19:24:17 -0400

    model update

--- src/models/responses.model.js
+++ src/models/responses.model.js
@@ -6,9 +6,13 @@ module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
   const responses = new Schema({
-    text: { type: String, required: true },
-    group: { type: Number, required: true },
-    questionId: {type: Number, required: true },
+    text: { type: String },
+    scaleA: {type: Number, min: 0, max: 100 },
+    scaleB: {type: Number, min: 0, max: 100 },
+    scaleC: {type: Number, min: 0, max: 100 },
+    scaleD: {type: Number, min: 0, max: 100 },
+    group: { type: Number, min: 1, max: 11, required: true },
+    questionId: {type: Number, min: 1, max: 3, required: true },
     questionType: {type: String, enum: ['text', 'scale'], required: true},
     isReviewed: {type: Boolean, required: true, default: false}
   }, {
